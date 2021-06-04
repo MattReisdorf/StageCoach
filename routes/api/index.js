27ed3d540954ac962392c5aaf3a21f9688f6c2b7
jonathan duty <jonathanduty2021@u.northwestern.edu>
2021-06-04 16:43:24 -0500

    oops that was just the client now pushing artist stuff

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const artistRoutes = require('./artistRoutes');
-const venueRoutes = require('./venueRoutes');
-const showRoutes = require('./showRoutes');
+const router = require("express").Router();
+const artistRoutes = require("./artistRoutes");
+const venueRoutes = require("./venueRoutes");
+const showRoutes = require("./showRoutes");
 
-router.use('/artists', artistRoutes);
-router.use('/venues', venueRoutes);
-router.use('/shows', showRoutes);
+router.use("/artists", artistRoutes);
+router.use("/venues", venueRoutes);
+router.use("/shows", showRoutes);
+
+module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,11 @@
-const sequelize = require('../config/connection');
-const { Artist, Venue, Show } = require('../models');
+require("dotenv").config();
+const sequelize = require("../config/connection");
 
-const artistData = require('./artistData.json');
-const venueData = require('./venueData.json')
-const showData = require('./showData.json')
+const { Artist, Venue, Show } = require("../models");
+
+const artistData = require("./artistData.json");
+const venueData = require("./venueData.json");
+const showData = require("./showData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -14,14 +16,14 @@ const seedDatabase = async () => {
   });
 
   const venues = await Venue.bulkCreate(venueData, {
-      individualHooks: true,
-      returning: true,
+    individualHooks: true,
+    returning: true,
   });
 
   const shows = await Show.bulkCreate(showData, {
     individualHooks: true,
     returning: true,
-});
+  });
 
   process.exit(0);
 };
